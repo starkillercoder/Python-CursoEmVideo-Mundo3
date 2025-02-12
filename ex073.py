0830d025b27de7041ed82cc3337e4c3a0cae1207
starkillercoder <eliwandejp@gmail.com>
2025-02-12 11:14:22 -0300

    Create ex073.py

---
+++ ex073.py
@@ -0,0 +1,6 @@
+membros = ('Im Nayeon', 'Yoo Jeongyeon', 'Hirai Momo',
+           'Minatozaki Sana', 'Park Jihyo', 'Myoi Mina',
+           'Kim Dahyun', 'Son Chaeyoung', 'Chou Tzuyu')
+print(f'As membros mais velhas são {membros[:5]}.')
+print(f'As membros mais novas são {membros[5:]}.')
+print(f'Son Chaeyoung está na {membros.index('Son Chaeyoung') + 1}ª posição.')
