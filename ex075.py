4db66f175277450438c448ab282631cc9cd17e64
starkillercoder <eliwandejp@gmail.com>
2025-02-14 10:40:40 -0300

    Update ex075.py

--- ex075.py
+++ ex075.py
@@ -1,13 +1,14 @@
-numero_1 = int(input('Digite um número: '))
-numero_2 = int(input('Digite outro número: '))
-numero_3 = int(input('Digite mais um número: '))
-numero_4 = int(input('Digite um último número: '))
-numeros = (numero_1, numero_2, numero_3, numero_4)
+numeros = (int(input('Digite um número: ')),
+            int(input('Digite outro número: ')),
+            int(input('Digite mais um número: ')),
+            int(input('Digite um último número: ')))
+print(f'Você digitou os valores {numeros}')
 print(f'O número 9 apareceu {numeros.count(9)} vezes.')
-print(f'O números 3 apareceu na posição {numeros.count(3)}')
+if 3 in numeros:
+    print(f'O números 3 apareceu na posição {numeros.index(3) + 1}.')
+else:
+    print('O número 3 não está na tupla.')
 print('Os números pares são:', end= ' ')
-for c in range(0, 4):
-    if numeros[c] % 2 == 0:
-        print(numeros[c],end='')
-        if c < 3:
-            print(',', end=' ')
+for n in numeros:
+    if n % 2 == 0:
+        print(n,end=' ')
