b589f524dcf346ab73b3cb17bf25161dd7da6973
starkillercoder <eliwandejp@gmail.com>
2025-02-15 20:39:05 -0300

    Update ex088.py

--- ex088.py
+++ ex088.py
@@ -10,12 +10,10 @@ for cont in range(0, jogos):
         while numero in copia:
             numero = randint(1, 60)
         copia.append(numero)
+    palpites.append(copia[:])
+    copia.clear()
 print(f'{f' SORTEANDO {jogos} JOGOS ':=^40}')
 for c in range(0, jogos):
-    print(f'{c + 1}º jogo: ', end='')
-    if c == 0:
-        print(sorted(copia[0:6]))
-    else:
-        print(sorted(copia[6 * c:6 * (c + 1)]))
+    print(f'{c + 1}º jogo: {sorted(palpites[c])}')
     sleep(1)
 print(f'{'BOA SORTE':=^40}')
