f26938e40cb6056d53921a04006acad9744b952a
starkillercoder <eliwandejp@gmail.com>
2025-02-14 21:33:24 -0300

    Update ex088.py

--- ex088.py
+++ ex088.py
@@ -5,21 +5,17 @@ palpites = []
 cont = 0
 jogos = int(input('Quantos jogos você quer que eu sorteie? '))
 for cont in range(0, jogos):
-    for c in range(0, 6):
+    for c in range(0,6):
         numero = randint(1, 60)
-        while numero in palpites:
+        while numero in copia:
             numero = randint(1, 60)
-        else:
-            copia.append(numero)
-        palpites.append(numero)
-        copia.clear()
-print(f'{f' Sorteando {jogos} jogos ':=^40}')
+        copia.append(numero)
+print(f'{f' SORTEANDO {jogos} JOGOS ':=^40}')
 for c in range(0, jogos):
-    print(f'{c + 1}º palpite: ', end=' ')
+    print(f'{c + 1}º jogo: ', end='')
     if c == 0:
-        print(sorted(palpites[0:6]))
-    if c > 0:
-        print(sorted(palpites[6 * c:6 * (c + 1)]))
-    cont += 6
-    sleep(0.5)
-print(f'{' BOA SORTE ':=^40}')
+        print(sorted(copia[0:6]))
+    else:
+        print(sorted(copia[6 * c:6 * (c + 1)]))
+    sleep(1)
+print(f'{'BOA SORTE':=^40}')
