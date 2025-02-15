ac394faf200a970694e446f1b2c42646f6112fae
starkillercoder <eliwandejp@gmail.com>
2025-02-15 20:28:11 -0300

    Update ex087.py

--- ex087.py
+++ ex087.py
@@ -1,26 +1,17 @@
 matriz = []
-p = 0
-c = 0
 cont = 0
 soma_pares = 0
-while True:
-    numero = int(input(f'Digite um número para a posição [{p}, {c}] '))
-    if numero % 2 == 0:
-        soma_pares += numero
-    matriz.append(numero)
-    c += 1
-    if c == 3:
-        c = 0
-        p += 1
-    if p == 3:
-        break
+for c in range(0, 3):
+    for cc in range(0,3):
+        numero = int(input(f'Digite um número para a posição [{c}, {cc}] '))
+        matriz.append(numero)
+        if numero % 2 == 0:
+            soma_pares += numero
 print('-=' * 30)
-for c in matriz:
-    if cont == 3 or cont == 6:
-        print(f'\n[ {c} ]', end='')
-    else:
-        print(f'[ {c} ]', end='')
-    cont += 1
+for c in range(0, 9):
+    print(f'[{matriz[c]:^5}]', end='')
+    if c == 2 or c == 5:
+        print()
 print(f'\n{'-=' * 30}')
 print(f'A soma dos valores pares é {soma_pares}')
 print(f'O maior valor da terceira coluna é {(matriz[2] + matriz[5] + matriz[8])}')
