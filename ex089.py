0aed8339ffa4a2bc02ea1a3d77a49cf55c92bd2f
starkillercoder <eliwandejp@gmail.com>
2025-02-16 11:50:29 -0300

    Update ex089.py

--- ex089.py
+++ ex089.py
@@ -1,27 +1,21 @@
-notas = []
-alunos = []
-sala = (alunos, notas)
-cont = 0
+sala = []
 while True:
-    nome = str(input('Nome do aluno: '))
-    alunos.append(nome)
-    nota1 = int(input('Nota 1: '))
-    notas.append(nota1)
-    nota2 = int(input('Nota 2: '))
-    notas.append(nota2)
+    nome = str(input('Nome: '))
+    n1 = float(input('Nota 1: '))
+    n2 = float(input('Nota 2: '))
+    media = (n1 + n2) / 2
+    sala.append([nome, [n1, n2], media])
     resposta = ' '
     while resposta not in 'SN':
-        resposta = str(input('Deseja continuar? [S/N]: ')).upper().strip()[0]
+        resposta = str(input('Deseja continuar? [S/N] ')).strip().upper()[0]
     if resposta == 'N':
         break
-print(f'{'No.':<3}', f'{'NOME':<20}', f'{'MÉDIA':<10}')
-for c in range(0, len(alunos)):
-    print(f'{c:<3}', f'{alunos[c]:<20}', f'{((notas[0 + cont] + notas[1 + cont]) / 2):<10}')
-    cont += 1
+print(f'{'No.':<4}{'Nome':<20}{'Média':>4}')
+for i, v in enumerate(sala):
+    print(f'{i:<4}{v[0]:<20}{v[2]:>4}')
 while True:
-    ver_notas = int(input('Mostrar notas de qual aluno? (999 para parar)'))
-    while ver_notas < 0 or ver_notas > cont - 1:
-        ver_notas = int(input('Mostrar notas de qual aluno? (999 para parar)'))
-    if ver_notas == 999:
+    notas = int(input('Mostrar notas de qual aluno? (999 para para)'))
+    if notas == 999:
         break
-    print(f'Notas de {alunos[ver_notas]} são: {notas[ver_notas], notas[ver_notas + 1]}')
+    if 0 <= notas <= len(sala) - 1:
+        print(f'Notas de {sala[notas][0]}: {sala[notas][1]}')
