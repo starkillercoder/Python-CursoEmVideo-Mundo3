d95188c90d99c66fedc148aa26fe7576094426ee
starkillercoder <eliwandejp@gmail.com>
2025-02-20 13:34:55 -0300

    Create ex103.py

---
+++ ex103.py
@@ -0,0 +1,12 @@
+def ficha(nome='<desconhecido>', gols=0):
+    print(f'O jogador {nome} fez {gols} gol(s).')
+n = str(input('Nome do jogador: '))
+g = str(input('Número de gols: '))
+if g.isnumeric():
+    g = int(g)
+else:
+    g = 0
+if n.strip() == '':
+    ficha(gols=g)
+else:
+    ficha(n, g)
